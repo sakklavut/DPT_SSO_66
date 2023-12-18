2554c15bc612c5eb3f7e0b607597d08229b68580
Kamonporn115 <120095052+Kamonporn115@users.noreply.github.com>
2023-12-18 17:15:56 +0700

    Update logo header_2.php

--- header_2.php
+++ header_2.php
@@ -31,7 +31,7 @@
     <div class="container">        
         <nav class="navbar navbar-expand-lg navbar-light ">
             <a class="navbar-brand" href="index.php">
-                <img src="assets/images/Logo_main.png" class="logo_main" alt="">
+                <img src="assets/images/Logo_main_1.png" class="logo_main" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
